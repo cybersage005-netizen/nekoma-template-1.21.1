8fb911e31de2bea9f776bfa89f5138e0e822d73f
Veer <cybersage005@gmail.com>
2026-02-11 07:24:29 +0530

    d

--- src/main/java/net/greenjab/nekomasfixed/trim/ModTrimPatterns.java
+++ src/main/java/net/greenjab/nekomasfixed/trim/ModTrimPatterns.java
@@ -1,4 +1,36 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package net.greenjab.nekomasfixed.trim;
 
+
+import net.minecraft.item.equipment.trim.ArmorTrimPattern;
+import net.minecraft.registry.Registerable;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+import org.jetbrains.annotations.UnknownNullability;
+
+
 public class ModTrimPatterns {
+    public static final RegistryKey<ArmorTrimPattern> WILD_FIRE_TRIM = of("wild_fire_trim");
+
+
+    public static void bootstrap(Registerable<ArmorTrimPattern> registry) {
+        register(registry, WILD_FIRE_TRIM);
+
+    }
+
+    private static void register(Registerable<ArmorTrimPattern> registry, @UnknownNullability RegistryKey<ArmorTrimPattern> key) {
+        ArmorTrimPattern armorTrimPattern = new ArmorTrimPattern(key.getValue(), Text.translatable(Util.createTranslationKey("trim_pattern", key.getValue())), false);
+        registry.register(key, armorTrimPattern);
+    }
+
+    private static RegistryKey<ArmorTrimPattern> of(String id) {
+        return RegistryKey.of(RegistryKeys.TRIM_PATTERN, Identifier.of("nekomasfixed", id));
+    }
 }
